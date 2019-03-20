58fe28d6caed5ee197a88ddf46f5753669ac3e04
Kyuhyoung Choi <kyuhyoung@gmail.com>
2019-03-20 21:50:29 +0900

    Finish threading.

--- python_cv2_cam_parallel_threading.py
+++ python_cv2_cam_parallel_threading.py
@@ -56,91 +56,122 @@ class DataClass:
         self.im_rgb = None
         self.fps_det = None
         self.fps_fetch = None
+        #'''        
         self.lock_rgb = Lock()
         #self.lock_bbox = Lock()
         self.lock_li_rgb = Lock()
         self.lock_li_det = Lock()
         self.lock_fps_det = Lock()
         self.lock_fps_fetch = Lock()
+        #'''
+
+
     def set_rgb(self, im_rgb):
         with self.lock_rgb:
-            self.im_rgb = im_rgb
+           self.im_rgb = im_rgb
+        #self.im_rgb = im_rgb
         return
     def get_rgb(self):
         with self.lock_rgb:
-            return self.im_rgb
+           return self.im_rgb
+        #return self.im_rgb
 
     def set_li_rgb(self, li_im_rgb):
         with self.lock_li_rgb:
             self.li_im_rgb = li_im_rgb
+        #self.li_im_rgb = li_im_rgb
     def get_li_rgb(self):
         with self.lock_li_rgb:
             return self.li_im_rgb
+        #return self.li_im_rgb
 
     def set_li_det(self, li_det):
         with self.lock_li_det:
             self.li_det = li_det
+        #self.li_det = li_det
     def get_li_det(self):
         with self.lock_li_det:
             return self.li_det
+        #return self.li_det
 
     def set_fps_det(self, fps_det):
         with self.lock_fps_det:
             self.fps_det = fps_det
+        #self.fps_det = fps_det
     def get_fps_det(self):
         with self.lock_fps_det:
             return self.fps_det
+        #return self.fps_det
 
     def set_fps_fetch(self, fps_fetch):
         with self.lock_fps_fetch:
             self.fps_fetch = fps_fetch
+        #self.fps_fetch = fps_fetch
     def get_fps_fetch(self):
         with self.lock_fps_fetch:
             return self.fps_fetch
+        #return self.fps_fetch
 
 def display_in_thread(class_data, COLORS):
 
     fps_disp = FPS().start()
+    is_huda = False
     while not class_data.end_of_capture:
 
         im_rgb = class_data.get_rgb()
+    
         if im_rgb is None:
-            continue
+            if is_huda:
+                class_data.end_of_capture = True
+                print('No more image for display')
+                print('No more image for display')
+                print('No more image for display')
+                print('No more image for display')
+                print('No more image for display')
+                print('im_rgb of display is NOT None')
+            else:
+                #print('First frame of display thread has not been arrived')
+                continue
+        is_huda = True
         hei, wid = im_rgb.shape[:2]
-        print('fps_disp._numFrames : ', fps_disp._numFrames)
+        #print('fps_disp._numFrames : ', fps_disp._numFrames)
         #print('hei : ', hei)
         #print('wid : ', wid)
         li_det = class_data.get_li_det()
         im_bgr = cv2.cvtColor(im_rgb, cv2.COLOR_RGB2BGR)
         #for i, det in enumerate(li_det):
-        for det in li_det:
+        for idx, det in enumerate(li_det):
+            #print('idx : ', idx)
             x, y, w, h = det.x, det.y, det.w, det.h
             color = [int(c) for c in COLORS[det.class_id]]
-            cv2.rectangle(im_bgr, (x, y), (x + w, y + h), color, 2)
+            cv2.rectangle(im_bgr, (x, y), (x + w, y + h), color, 1)
             text = "{}: {:.4f}".format(det.label, det.confidence)
-            cv2.putText(im_bgr, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            cv2.putText(im_bgr, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
         fps_det = class_data.get_fps_det()
-        if fps_det is not None:
+        if fps_det:
             text = "fps det : {:.1f}".format(fps_det)
-            cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            #print("fps det in display thread : {:.1f}".format(fps_det))
+            cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 1)
         
         fps_fetch = class_data.get_fps_fetch()
         if fps_fetch is not None:
             text = "fps fetch : {:.1f}".format(fps_fetch)
-            cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            #print("fps fetch in display thread : {:.1f}".format(fps_fetch))
+            cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1)
 
         fps_disp.update();
         text = "fps disp : {:.1f}".format(fps_disp.fps())
-        cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 70), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
         cv2.imshow('im_bgr', im_bgr)
         k = cv2.waitKey(1) & 0xFF
         if k == 27: # esc key
-            cv2.destroyAllWindow()
-            class_data.end_of_caputre = True
+            cv2.destroyAllWindows()
+            class_data.end_of_capture = True
         #elif k = ord('s'): # 's' key
             #cv2.imwrite('lenagray.png',img)
             #cv2.destroyAllWindow()
+        #print('fps_display : ', fps_disp.fps())
     print("class_data.end_of_capture is True : display_in_thread") 
 
 def detect_in_thread(class_data, dir_yolo, th_confidence, th_nms_iou, LABEL):
@@ -165,20 +196,20 @@ def detect_in_thread(class_data, dir_yolo, th_confidence, th_nms_iou, LABEL):
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     print("[INFO] ln finished ..."); #exit()
     fps_det = FPS().start()
-    print('class_data.end_of_capture : ', class_data.end_of_capture); #exit()
+    print('class_data.end_of_capture of detect in thread : ', class_data.end_of_capture); #exit()
+    is_huda = False
     while not class_data.end_of_capture:
         im_rgb = class_data.get_rgb()
         #print("im_rgb : ", im_rgb)
-        if im_rgb is not None:
-            is_huda = True
-            #print('is_huda : ', is_huda); #exit()
-        else:
+        if im_rgb is None:
             if is_huda:
                 class_data.end_of_capture = True
+                print('class_data.end_of_capture of detect in thread is True'); #exit()
             continue
+        is_huda = True
         #print('is_huda : ', is_huda); #exit()
         blob = cv2.dnn.blobFromImage(im_rgb, 1 / 255.0, (416, 416), swapRB=False, crop=False)
-        print('fps_det._numFrames : ', fps_det._numFrames); #exit()
+        #print('fps_det._numFrames : ', fps_det._numFrames); #exit()
         H, W = im_rgb.shape[:2]
         net.setInput(blob)
         start = time.time()
@@ -249,20 +280,24 @@ def detect_in_thread(class_data, dir_yolo, th_confidence, th_nms_iou, LABEL):
                 #cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         class_data.set_li_det(li_det)
         fps_det.update();   class_data.set_fps_det(fps_det.fps())
+        #print('fps_det : ', fps_det.fps())
     print("class_data.end_of_capture is True : detect_in_thread") 
         
-def fetch_in_thread(class_data, fn_video_or_cam, len_li_rgb):
+def fetch_in_thread(class_data, fn_video_or_cam, len_li_rgb, is_video):
     li_rgb = []
     is_huda = False
     print("fn_video_or_cam : ", fn_video_or_cam)
     kapture = cv2.VideoCapture(fn_video_or_cam)
+    #kapture = cv2.VideoCapture(1)
     class_data.end_of_capture = not kapture.isOpened()
+    if is_video:
+        n_frame_total = int(kapture.get(cv2.CAP_PROP_FRAME_COUNT))
     fps_fetch = FPS().start()
     while not class_data.end_of_capture:
+        #print('class_data.end_of_capture is True : fetch thread');
         ret, im_bgr = kapture.read()
         if ret:
-            if not is_huda:
-                is_huda = True
+            is_huda = True
             im_rgb = class_data.set_rgb(cv2.cvtColor(im_bgr, cv2.COLOR_BGR2RGB))
             li_rgb.append(im_rgb)
             if len(li_rgb) >= len_li_rgb:
@@ -270,10 +305,16 @@ def fetch_in_thread(class_data, fn_video_or_cam, len_li_rgb):
                 del li_rgb[:]
         else:
             if is_huda:
-                class_data.end_of_caputre = True 
+                class_data.end_of_capture = True 
+        if is_video:
+            idx_frame = int(kapture.get(cv2.CAP_PROP_POS_FRAMES))
+            if idx_frame >= n_frame_total - 1:
+                class_data.end_of_capture = True
 
-        print('fps_fetch._numFrames : ', fps_fetch._numFrames)
+        #print('fps_fetch._numFrames : ', fps_fetch._numFrames)
         fps_fetch.update();   class_data.set_fps_fetch(fps_fetch.fps())
+        #print('fps_fetch : ', fps_fetch.fps())
+   
     print("class_data.end_of_capture is True : fetch_in_thread") 
 
 
@@ -298,11 +339,16 @@ ap.add_argument("-i", "--input", required=True,
 #def main():
 #fn_video = "C:/Users/kevin/Videos/sunny_moon/20190125_125054.mp4"
 dir_yolo = "data"
+'''
 #fn_video = "data/20190125_125054.mp4"
-fn_video = "data/test.mp4"
+#fn_video = "data/test.mp4"
+is_this_video = True
+'''
+fn_video = 0
+is_this_video = False
+
 th_confidence = 0.5
 th_nms_iou = 0.3
-
 
 # load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([dir_yolo, "coco.names"])
@@ -314,12 +360,73 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
 
 class_data = DataClass()
-thread_fetch  = Thread(target = fetch_in_thread, args = (class_data, fn_video, 8))
+thread_fetch  = Thread(target = fetch_in_thread, args = (class_data, fn_video, 8, is_this_video))
 thread_fetch.start()
 thread_detect  = Thread(target = detect_in_thread, args = (class_data, dir_yolo, th_confidence, th_nms_iou, LABELS))
 thread_detect.start()
+
 thread_display  = Thread(target = display_in_thread, args = (class_data, COLORS))
 thread_display.start()
+#display_in_thread(class_data, COLORS)
+
+
+'''
+fps_disp = FPS().start()
+is_huda = False
+while not class_data.end_of_capture:
+
+    im_rgb = class_data.get_rgb()
+    
+    if im_rgb is None:
+        if is_huda:
+            class_data.end_of_capture = True
+            print('im_rgb of display is NOT None')
+        else:
+            #print('First frame of display thread has not been arrived')
+            continue
+    is_huda = True
+    hei, wid = im_rgb.shape[:2]
+    print('fps_disp._numFrames : ', fps_disp._numFrames)
+    #print('hei : ', hei)
+    #print('wid : ', wid)
+    li_det = class_data.get_li_det()
+    im_bgr = cv2.cvtColor(im_rgb, cv2.COLOR_RGB2BGR)
+    #for i, det in enumerate(li_det):
+    for idx, det in enumerate(li_det):
+        print('idx : ', idx)
+        x, y, w, h = det.x, det.y, det.w, det.h
+        color = [int(c) for c in COLORS[det.class_id]]
+        cv2.rectangle(im_bgr, (x, y), (x + w, y + h), color, 2)
+        text = "{}: {:.4f}".format(det.label, det.confidence)
+        cv2.putText(im_bgr, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    fps_det = class_data.get_fps_det()
+    if fps_det:
+        text = "fps det : {:.1f}".format(fps_det)
+        print("fps det in display thread : {:.1f}".format(fps_det))
+        cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        
+    fps_fetch = class_data.get_fps_fetch()
+    if fps_fetch is not None:
+        text = "fps fetch : {:.1f}".format(fps_fetch)
+        print("fps fetch in display thread : {:.1f}".format(fps_fetch))
+        cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    fps_disp.update();
+    text = "fps disp : {:.1f}".format(fps_disp.fps())
+    cv2.putText(im_bgr, text, (int(wid * 0.4), hei - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.imshow('im_bgr', im_bgr)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27: # esc key
+        cv2.destroyAllWindow()
+        class_data.end_of_capture = True
+        print('class_data.end_of_capture is True')
+    print('fps_display : ', fps_disp.fps())
+print("class_data.end_of_capture is True : display_in_thread") 
+'''
+
+
+
 
 thread_fetch.join()
 thread_detect.join()
